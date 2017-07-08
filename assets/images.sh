ba2a02a7f8954554fcd05e52d08d98e9e2b2b4a6
lieu <lieuzhenghong@gmail.com>
2017-07-08 11:10:57 +0800

    Add placeholder images and clean up

---
+++ assets/images.sh
@@ -0,0 +1,3 @@
+convert -background 'rgba(240, 240, 240, 1.0)' -fill 'rgba(150, 150, 150, 0.8)' -size 300x300 -draw 'line 0,0 120,120 line 0,300 120,180 line 300,0 180,120 line 300,300 180,180' -pointsize 36 -gravity center label:'Image preview' placeholder.png
+convert -background 'rgba(240, 240, 240, 1.0)' -fill 'rgba(150, 150, 150, 0.8)' -size 800x600 -pointsize 36 -gravity center label:'Upload floor plan' canvas_placeholder.png
+
